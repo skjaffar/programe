b7b81e494d1fc8094819f14770f15995f230a52b
Shaik Jaffar <107207838+skjaffar@users.noreply.github.com>
2022-07-25 22:07:38 +0530

    Add files via upload

---
+++ 03_area_of_circle.py
@@ -0,0 +1,33 @@
+"""
+# r=float(input("Enter the Radius:"))
+# PI=22/7
+# Area=PI*r*r
+# print("The Area of the Circle is %.2f" %Area)
+
+# """
+
+# """ Another Method """
+# # """
+# import math
+# r=float(input("Enter the Radius:"))
+# Area=math.pi*r**2
+# print("The Area of the Circle is:%.2f" %Area)
+# """
+
+# import math
+# print(math.pi)
+
+
+
+
+# r=float(input("enter the value of radious"))
+# PI=22/7
+# area= PI*r*r
+# print("the area of circle is:%.2f" %(area))
+
+
+
+r=float(input("enter the radious:"))
+PI=22/7
+area=PI*r*r
+print("area of a circle is:%.2f" %area)
